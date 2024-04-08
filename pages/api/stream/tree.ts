abdac2a11eb4a762f33af638b74418af1fe26a0e
Chonwai <chonwaiun@gmail.com>
2024-04-09 01:43:59 +0800

    Add: Add useCompletion hook for API call

--- pages/api/stream/tree.ts
+++ pages/api/stream/tree.ts
@@ -1,100 +1,190 @@
 // pages/api/tree.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { StreamingTextResponse } from 'ai';
+import { createParser, ParsedEvent, ReconnectInterval } from 'eventsource-parser';
+import { StreamingTextResponse, streamToResponse } from 'ai';
 
-// export const runtime = 'edge'; // or 'nodejs' which uses Serverless Functions
-// export const dynamic = 'force-dynamic'; // always run dynamically
+export const runtime = 'edge'; // or 'nodejs' which uses Serverless Functions
+export const dynamic = 'force-dynamic'; // always run dynamically
 
 export const config = {
-    supportsResponseStreaming: true
-    // runtime: 'edge',
-    // dynamic: 'force-dynamic'
+    supportsResponseStreaming: true,
+    runtime: 'edge',
+    dynamic: 'force-dynamic'
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        try {
-            res.setHeader('Content-Type', 'application/json');
-            res.setHeader('Cache-Control', 'no-cache, no-transform');
-            res.setHeader('Connection', 'keep-alive');
-            res.setHeader('accept', 'text/event-stream');
-            // const Uint8ArrayToString = (fileData: any) => {
-            //     const utf8 = Array.from(fileData)
-            //         .map((item: any) => String.fromCharCode(item))
-            //         .join('');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 'no-cache, no-transform');
+    res.setHeader('Connection', 'keep-alive');
+    res.setHeader('accept', 'text/event-stream');
+    // const Uint8ArrayToString = (fileData: any) => {
+    //     const utf8 = Array.from(fileData)
+    //         .map((item: any) => String.fromCharCode(item))
+    //         .join('');
 
-            //     return decodeURIComponent(escape(utf8));
-            // };
+    //     return decodeURIComponent(escape(utf8));
+    // };
 
-            // const fetchStream = (url: any, params: any) => {
-            //     const { onmessage, onclose, ...otherParams } = params;
+    // const fetchStream = (url: any, params: any) => {
+    //     const { onmessage, onclose, ...otherParams } = params;
 
-            //     const push = async (controller: any, reader: any) => {
-            //         const { value, done } = await reader.read();
-            //         if (done) {
-            //             controller.close();
-            //             onclose?.();
-            //         } else {
-            //             onmessage?.(Uint8ArrayToString(value));
-            //             controller.enqueue(value);
-            //             push(controller, reader);
-            //         }
-            //     };
+    //     const push = async (controller: any, reader: any) => {
+    //         const { value, done } = await reader.read();
+    //         if (done) {
+    //             controller.close();
+    //             onclose?.();
+    //         } else {
+    //             onmessage?.(Uint8ArrayToString(value));
+    //             controller.enqueue(value);
+    //             push(controller, reader);
+    //         }
+    //     };
 
-            //     return fetch(url, otherParams).then((response: any) => {
-            //         const reader = response.body.getReader();
-            //         return new ReadableStream({
-            //             start(controller) {
-            //                 push(controller, reader);
-            //             }
-            //         });
-            //     });
-            // };
+    //     return fetch(url, otherParams).then((response: any) => {
+    //         const reader = response.body.getReader();
+    //         return new ReadableStream({
+    //             start(controller) {
+    //                 push(controller, reader);
+    //             }
+    //         });
+    //     });
+    // };
 
-            // const stream = await fetchStream(
-            //     `${process.env.NEXT_PUBLIC_LCEL_FAST_API}/generate/search_documents/tree/stream`,
-            //     {
-            //         method: 'POST',
-            //         body: JSON.stringify(req.body),
-            //         headers: {
-            //             Connection: 'keep-alive',
-            //             accept: 'text/event-stream',
-            //             'Content-Type': 'application/json'
-            //         },
-            //         onmessage: (response: any) => {
-            //             console.log(response);
-            //             // res.write(response);
-            //         }
-            //     }
-            // );
+    // const stream = await fetchStream(
+    //     `${process.env.NEXT_PUBLIC_LCEL_FAST_API}/generate/search_documents/tree/stream`,
+    //     {
+    //         method: 'POST',
+    //         body: JSON.stringify(req.body),
+    //         headers: {
+    //             Connection: 'keep-alive',
+    //             accept: 'text/event-stream',
+    //             'Content-Type': 'application/json'
+    //         },
+    //         onmessage: (response: any) => {
+    //             console.log(response);
+    //             // res.write(response);
+    //         }
+    //     }
+    // );
 
-            // return new StreamingTextResponse(stream);
-            // return res.send(stream);
+    // return new StreamingTextResponse(stream);
+    // return res.send(stream);
 
-            const response = await fetch(
-                `${process.env.NEXT_PUBLIC_LCEL_FAST_API}/generate/search_documents/tree/stream`,
-                {
-                    headers: {
-                        Connection: 'keep-alive',
-                        accept: 'text/event-stream',
-                        'Content-Type': 'application/json'
-                    },
-                    method: 'POST',
-                    body: JSON.stringify(req.body)
-                }
-            );
+    let counter = 0;
+    const encoder = new TextEncoder();
+    const decoder = new TextDecoder();
 
-            const json = await response.text();
-
-            console.log(json);
-
-            res.status(200).send(json);
-        } catch (error) {
-            console.error('Error:', error);
-            res.status(500).json({ message: 'Internal Server Error' });
+    const response: any = await fetch(
+        `${process.env.NEXT_PUBLIC_LCEL_FAST_API}/generate/search_documents/tree/stream`,
+        {
+            headers: {
+                Connection: 'keep-alive',
+                accept: 'text/event-stream',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(req.body)
         }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+    );
+
+    const stream = new ReadableStream({
+        async start(controller) {
+            function onParse(event: ParsedEvent | ReconnectInterval) {
+                if (event.type === 'event') {
+                    const data = event.data;
+                    // console.log(data);
+                    if (data === '[DONE]') {
+                        controller.close();
+                        return;
+                    }
+                    try {
+                        // const json = JSON.parse(data);
+                        // console.log(json);
+                        // const text = json.choices[0].text;
+                        console.log(data);
+                        const text = data;
+                        // res.write(text);
+                        // if (counter < 2 && (text.match(/\n/) || []).length) {
+                        //     return;
+                        // }
+                        const queue = encoder.encode(text);
+                        // const queue = encoder.encode(json);
+                        controller.enqueue(queue);
+                        counter++;
+                    } catch (e) {
+                        controller.error(e);
+                    }
+                }
+            }
+
+            // stream response (SSE) from OpenAI may be fragmented into multiple chunks
+            // this ensures we properly read chunks & invoke an event for each SSE event stream
+            const parser = createParser(onParse);
+
+            // https://web.dev/streams/#asynchronous-iteration
+            for await (const chunk of response.body as any) {
+                // console.log('Chunk: ', chunk);
+                parser.feed(decoder.decode(chunk));
+            }
+        }
+    });
+
+    // const stream = new ReadableStream({
+    //     async start(controller) {
+    //         const decoder = new TextDecoder('utf-8');
+    //         let buffer = '';
+
+    //         async function processData(chunk: any) {
+    //             console.log('Chunk: ', chunk);
+    //             buffer += chunk;
+    //             console.log('Buffer: ', buffer);
+    //             const boundary = buffer.lastIndexOf('\n');
+
+    //             if (boundary !== -1) {
+    //                 const content = buffer.substring(0, boundary);
+    //                 buffer = buffer.substring(boundary + 1); // 把剩余的数据保存到buffer中
+
+    //                 for (const data of content.split('\n')) {
+    //                     if (data.trim()) {
+    //                         try {
+    //                             const json = JSON.parse(data);
+    //                             controller.enqueue(
+    //                                 new TextEncoder().encode(JSON.stringify(json) + '\n')
+    //                             );
+    //                         } catch (e) {
+    //                             controller.error(`Error parsing JSON: ${e}`);
+    //                             controller.close();
+    //                             break;
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+
+    //         try {
+    //             const reader = response.body.getReader();
+    //             let done, value;
+    //             while ((({ done, value } = await reader.read()), !done)) {
+    //                 const chunk = decoder.decode(value, { stream: true }); // 将Uint8Array解码为字符串
+    //                 await processData(chunk);
+    //             }
+    //             await processData(decoder.decode()); // 确保处理所有剩余数据
+    //             controller.close(); // 所有数据读取完毕，关闭控制器
+    //         } catch (error) {
+    //             controller.error(`Stream error: ${error}`);
+    //         }
+    //     }
+    // });
+
+    // const json = await response.text();
+
+    // console.log(json);
+
+    // res.status(200).send(json);
+
+    console.log(stream);
+
+    // return new StreamingTextResponse(stream);
+    streamToResponse(stream, res);
+    // res.status(200).send(stream);
 }
